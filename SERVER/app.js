42e09eda1e94253de621d27549582b437ebc5c25
gkartikey05 <gkartikey05@gmail.com>
2025-05-18 15:35:20 +0530

    Updated cors

--- SERVER/app.js
+++ SERVER/app.js
@@ -5,19 +5,30 @@ import morgan from "morgan";
 import { config } from "dotenv";
 import userRoutes from "./routes/user.routes.js";
 import transactionRoutes from "./routes/transaction.routes.js";
-import errorMiddleware from './middleware/error.middleware.js'
+import errorMiddleware from "./middleware/error.middleware.js";
 
 config();
 const app = express();
-const FRONTEND_URL = process.env.FRONTEND_URL_PROD || process.env.FRONTEND_URL_DEV
+const allowedOrigins = [
+  process.env.FRONTEND_URL_PROD,
+  process.env.FRONTEND_URL_DEV,
+];
 
-app.use(express.json());
 app.use(
   cors({
-    origin: [FRONTEND_URL],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
@@ -29,6 +40,6 @@ app.use("/ping", function (req, res) {
 app.use("/api/v1/user", userRoutes);
 app.use("api/v1/transaction", transactionRoutes);
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 export default app;
